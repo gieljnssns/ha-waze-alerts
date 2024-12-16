8f22b441c9ea7c6d9b4861158716e4c4e189742b
gieljnssns <gieljnssns@me.com>
2024-12-16 10:08:51 +0000

    init2

--- custom_components/ha_waze_alerts/config_flow.py
+++ custom_components/ha_waze_alerts/config_flow.py
@@ -1,11 +1,14 @@
 """Adds config flow for Blueprint."""
 
-from __future__ import annotations
+# from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.const import CONF_MONITORED_CONDITIONS, CONF_RADIUS
 from homeassistant.core import callback
-from .const import DOMAIN, CATEGORIES
+
+from .const import CATEGORIES, DEFAULT_RADIUS, DOMAIN
+
 
 class WazeAlertsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Location Tracker."""
@@ -20,17 +23,15 @@ class WazeAlertsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="Location Tracker", data=user_input)
 
         # Haal beschikbare device_trackers op
-        device_trackers = [
-            entity_id for entity_id in self.hass.states.async_entity_ids("device_tracker")
-        ]
+        device_trackers = list(self.hass.states.async_entity_ids("device_tracker"))
 
         # Stel het formulier samen
         schema = vol.Schema(
             {
                 vol.Required("device_tracker"): vol.In(device_trackers),
-                vol.Required("radius", default=100): vol.Coerce(int),
-                vol.Required("categories", default=[]): vol.MultipleChoice(
-                    options=CATEGORIES
+                vol.Required(CONF_RADIUS): vol.Coerce(int),
+                vol.Required(CONF_MONITORED_CONDITIONS): vol.All(
+                    [vol.In(CATEGORIES)], vol.Length(min=1)
                 ),
             }
         )
@@ -55,12 +56,14 @@ class WazeAlertsOptionsFlowHandler(config_entries.OptionsFlow):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        # Haal huidige configuratie op
         options = {
-            vol.Required("radius", default=self.config_entry.options.get("radius", 100)): vol.Coerce(int),
-            vol.Required("categories", default=self.config_entry.options.get("categories", [])): vol.MultipleChoice(
-                options=CATEGORIES
-            ),
+            vol.Required(
+                CONF_RADIUS, default=self.config_entry.options.get(CONF_RADIUS, 1000)
+            ): vol.Coerce(int),
+            vol.Required(
+                CONF_MONITORED_CONDITIONS,
+                default=self.config_entry.options.get(CONF_MONITORED_CONDITIONS, []),
+            ): vol.All([vol.In(CATEGORIES)], vol.Length(min=1)),
         }
 
         return self.async_show_form(step_id="init", data_schema=vol.Schema(options))
--- custom_components/ha_waze_alerts/const.py
+++ custom_components/ha_waze_alerts/const.py
@@ -2,10 +2,25 @@
 
 from logging import Logger, getLogger
 
+import voluptuous as vol
+
 LOGGER: Logger = getLogger(__package__)
 
 DOMAIN = "ha_waze_alerts"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-DEFAULT_RADIUS = 1000 #in meters
+DEFAULT_RADIUS = 1000  # in meters
 
-CATEGORIES = ["ACCIDENT", "JAM", "POLICE", "WEATHERHAZARD", "HAZARD", "CONSTRUCTION", "ROAD_CLOSED"]
+CATEGORIES = [
+    "ACCIDENT",
+    "JAM",
+    "POLICE",
+    "WEATHERHAZARD",
+    "HAZARD",
+    "CONSTRUCTION",
+    "ROAD_CLOSED",
+]
+
+categories_schema = vol.All(
+    [vol.In(CATEGORIES)],  # Valideer dat elk item in de lijst een geldige categorie is
+    vol.Length(min=1),  # Vereist minimaal 1 selectie
+)
